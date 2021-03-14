d0b6fcea9e3ec2219910c6f5da5adfd06daa43ff
farhadpesaraklu <farhadpesaraklu@gmail.com>
2021-03-14 16:21:41 +0330

    add row

--- src/components/ChatRow.tsx
+++ src/components/ChatRow.tsx
@@ -4,7 +4,279 @@ const ChatRow: FC = () => {
   return (
     <div>
       <div>
-        <div className="is-flex chatRowContainer has-background-primary-light is-justify-content-space-between is-align-items-center  ">
+        <div className="chatRowContainer is-flex is-justify-content-space-between is-align-items-center  ">
+          <div className="is-flex is-align-items-center">
+            <div className="is-flex is-align-items-center">
+              <img className="chatPageHeaderImage m-2 " alt="" />
+            </div>
+            <div className="">
+              <p className="title is-5 is-size-5 ">Farhad</p>
+              <p className="subtitle is-size-7 ">Hello, How yo doing?</p>
+            </div>
+          </div>
+          <div className=" mr-2 ">
+            <p >Sunday</p>
+            <p >15:15</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="is-flex chatRowContainer  is-justify-content-space-between is-align-items-center  ">
+          <div className="is-flex is-align-items-center">
+            <div className="is-flex is-align-items-center">
+              <img className="chatPageHeaderImage m-2 " alt="" />
+            </div>
+            <div className="">
+              <p className="title is-5 is-size-5 ">Farhad</p>
+              <p className="subtitle is-size-7 ">Hello, How yo doing?</p>
+            </div>
+          </div>
+          <div className=" mr-2 ">
+            <p >Sunday</p>
+            <p >15:15</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="is-flex chatRowContainer  is-justify-content-space-between is-align-items-center  ">
+          <div className="is-flex is-align-items-center">
+            <div className="is-flex is-align-items-center">
+              <img className="chatPageHeaderImage m-2 " alt="" />
+            </div>
+            <div className="">
+              <p className="title is-5 is-size-5 ">Farhad</p>
+              <p className="subtitle is-size-7 ">Hello, How yo doing?</p>
+            </div>
+          </div>
+          <div className=" mr-2 ">
+            <p >Sunday</p>
+            <p >15:15</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="is-flex chatRowContainer  is-justify-content-space-between is-align-items-center  ">
+          <div className="is-flex is-align-items-center">
+            <div className="is-flex is-align-items-center">
+              <img className="chatPageHeaderImage m-2 " alt="" />
+            </div>
+            <div className="">
+              <p className="title is-5 is-size-5 ">Farhad</p>
+              <p className="subtitle is-size-7 ">Hello, How yo doing?</p>
+            </div>
+          </div>
+          <div className=" mr-2 ">
+            <p >Sunday</p>
+            <p >15:15</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="is-flex chatRowContainer  is-justify-content-space-between is-align-items-center  ">
+          <div className="is-flex is-align-items-center">
+            <div className="is-flex is-align-items-center">
+              <img className="chatPageHeaderImage m-2 " alt="" />
+            </div>
+            <div className="">
+              <p className="title is-5 is-size-5 ">Farhad</p>
+              <p className="subtitle is-size-7 ">Hello, How yo doing?</p>
+            </div>
+          </div>
+          <div className=" mr-2 ">
+            <p >Sunday</p>
+            <p >15:15</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="is-flex chatRowContainer  is-justify-content-space-between is-align-items-center  ">
+          <div className="is-flex is-align-items-center">
+            <div className="is-flex is-align-items-center">
+              <img className="chatPageHeaderImage m-2 " alt="" />
+            </div>
+            <div className="">
+              <p className="title is-5 is-size-5 ">Farhad</p>
+              <p className="subtitle is-size-7 ">Hello, How yo doing?</p>
+            </div>
+          </div>
+          <div className=" mr-2 ">
+            <p >Sunday</p>
+            <p >15:15</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="is-flex chatRowContainer  is-justify-content-space-between is-align-items-center  ">
+          <div className="is-flex is-align-items-center">
+            <div className="is-flex is-align-items-center">
+              <img className="chatPageHeaderImage m-2 " alt="" />
+            </div>
+            <div className="">
+              <p className="title is-5 is-size-5 ">Farhad</p>
+              <p className="subtitle is-size-7 ">Hello, How yo doing?</p>
+            </div>
+          </div>
+          <div className=" mr-2 ">
+            <p >Sunday</p>
+            <p >15:15</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="is-flex chatRowContainer  is-justify-content-space-between is-align-items-center  ">
+          <div className="is-flex is-align-items-center">
+            <div className="is-flex is-align-items-center">
+              <img className="chatPageHeaderImage m-2 " alt="" />
+            </div>
+            <div className="">
+              <p className="title is-5 is-size-5 ">Farhad</p>
+              <p className="subtitle is-size-7 ">Hello, How yo doing?</p>
+            </div>
+          </div>
+          <div className=" mr-2 ">
+            <p >Sunday</p>
+            <p >15:15</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="is-flex chatRowContainer  is-justify-content-space-between is-align-items-center  ">
+          <div className="is-flex is-align-items-center">
+            <div className="is-flex is-align-items-center">
+              <img className="chatPageHeaderImage m-2 " alt="" />
+            </div>
+            <div className="">
+              <p className="title is-5 is-size-5 ">Farhad</p>
+              <p className="subtitle is-size-7 ">Hello, How yo doing?</p>
+            </div>
+          </div>
+          <div className=" mr-2 ">
+            <p >Sunday</p>
+            <p >15:15</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="is-flex chatRowContainer  is-justify-content-space-between is-align-items-center  ">
+          <div className="is-flex is-align-items-center">
+            <div className="is-flex is-align-items-center">
+              <img className="chatPageHeaderImage m-2 " alt="" />
+            </div>
+            <div className="">
+              <p className="title is-5 is-size-5 ">Farhad</p>
+              <p className="subtitle is-size-7 ">Hello, How yo doing?</p>
+            </div>
+          </div>
+          <div className=" mr-2 ">
+            <p >Sunday</p>
+            <p >15:15</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="is-flex chatRowContainer  is-justify-content-space-between is-align-items-center  ">
+          <div className="is-flex is-align-items-center">
+            <div className="is-flex is-align-items-center">
+              <img className="chatPageHeaderImage m-2 " alt="" />
+            </div>
+            <div className="">
+              <p className="title is-5 is-size-5 ">Farhad</p>
+              <p className="subtitle is-size-7 ">Hello, How yo doing?</p>
+            </div>
+          </div>
+          <div className=" mr-2 ">
+            <p >Sunday</p>
+            <p >15:15</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="is-flex chatRowContainer  is-justify-content-space-between is-align-items-center  ">
+          <div className="is-flex is-align-items-center">
+            <div className="is-flex is-align-items-center">
+              <img className="chatPageHeaderImage m-2 " alt="" />
+            </div>
+            <div className="">
+              <p className="title is-5 is-size-5 ">Farhad</p>
+              <p className="subtitle is-size-7 ">Hello, How yo doing?</p>
+            </div>
+          </div>
+          <div className=" mr-2 ">
+            <p >Sunday</p>
+            <p >15:15</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="is-flex chatRowContainer  is-justify-content-space-between is-align-items-center  ">
+          <div className="is-flex is-align-items-center">
+            <div className="is-flex is-align-items-center">
+              <img className="chatPageHeaderImage m-2 " alt="" />
+            </div>
+            <div className="">
+              <p className="title is-5 is-size-5 ">Farhad</p>
+              <p className="subtitle is-size-7 ">Hello, How yo doing?</p>
+            </div>
+          </div>
+          <div className=" mr-2 ">
+            <p >Sunday</p>
+            <p >15:15</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="is-flex chatRowContainer  is-justify-content-space-between is-align-items-center  ">
+          <div className="is-flex is-align-items-center">
+            <div className="is-flex is-align-items-center">
+              <img className="chatPageHeaderImage m-2 " alt="" />
+            </div>
+            <div className="">
+              <p className="title is-5 is-size-5 ">Farhad</p>
+              <p className="subtitle is-size-7 ">Hello, How yo doing?</p>
+            </div>
+          </div>
+          <div className=" mr-2 ">
+            <p >Sunday</p>
+            <p >15:15</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="is-flex chatRowContainer  is-justify-content-space-between is-align-items-center  ">
+          <div className="is-flex is-align-items-center">
+            <div className="is-flex is-align-items-center">
+              <img className="chatPageHeaderImage m-2 " alt="" />
+            </div>
+            <div className="">
+              <p className="title is-5 is-size-5 ">Farhad</p>
+              <p className="subtitle is-size-7 ">Hello, How yo doing?</p>
+            </div>
+          </div>
+          <div className=" mr-2 ">
+            <p >Sunday</p>
+            <p >15:15</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="is-flex chatRowContainer  is-justify-content-space-between is-align-items-center  ">
+          <div className="is-flex is-align-items-center">
+            <div className="is-flex is-align-items-center">
+              <img className="chatPageHeaderImage m-2 " alt="" />
+            </div>
+            <div className="">
+              <p className="title is-5 is-size-5 ">Farhad</p>
+              <p className="subtitle is-size-7 ">Hello, How yo doing?</p>
+            </div>
+          </div>
+          <div className=" mr-2 ">
+            <p >Sunday</p>
+            <p >15:15</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="is-flex chatRowContainer  is-justify-content-space-between is-align-items-center  ">
           <div className="is-flex is-align-items-center">
             <div className="is-flex is-align-items-center">
               <img className="chatPageHeaderImage m-2 " alt="" />
