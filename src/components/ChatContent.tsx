4926a8da71e93bd7fb01e6bda9237cba87c67637
farhadpesaraklu <farhadpesaraklu@gmail.com>
2021-04-05 16:19:19 +0430

    remove & edit chat content

--- src/components/ChatContent.tsx
+++ src/components/ChatContent.tsx
@@ -1,48 +1,24 @@
-export default function ChatContent() {
+const ChatContent: React.FC = () => {
   return (
     <div className="chatContentContainer is-flex is-flex-direction-column">
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >Hi!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >asdfghsdfhgsdfgh!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >Hifdghhhh!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >Hrtttttttttttttttttttti!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >Hi!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >asdfghsdfhgsdfgh!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >Hifdghhhh!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >Hrtttttttttttttttttttti!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >Hi!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >asdfghsdfhgsdfgh!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >Hifdghhhh!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >Hrtttttttttttttttttttti!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >Hi!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >asdfghsdfhgsdfgh!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >Hifdghhhh!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >Hrtttttttttttttttttttti!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >Hi!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >asdfghsdfhgsdfgh!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >Hifdghhhh!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >Hrtttttttttttttttttttti!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >Hi!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >asdfghsdfhgsdfgh!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >Hifdghhhh!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >Hrtttttttttttttttttttti!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >Hi!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >asdfghsdfhgsdfgh!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >Hifdghhhh!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >Hrtttttttttttttttttttti!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >Hi!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >asdfghsdfhgsdfgh!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >Hifdghhhh!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >Hrtttttttttttttttttttti!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >Hi!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >asdfghsdfhgsdfgh!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >Hifdghhhh!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >Hrtttttttttttttttttttti!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >Hi!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >asdfghsdfhgsdfgh!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >Hifdghhhh!</span>
-      <span className="receivedMessageStyle p-1 mx-4 my-2" >Hrtttttttttttttttttttti!</span>
-
+      <span className="receivedMessageStyle p-1 mx-4 my-2">
+        <b className="has-text-link">Farhad:</b> Hi everyone!
+      </span>
+      <span className="receivedMessageStyle p-1 mx-4 my-2">
+        <b className="has-text-link">Farhad:</b> it makes me happy to be among
+        you :)
+      </span>
+      <span className="sentMessageStyle p-1 mx-4 my-2">
+        <b className="has-text-danger-dark">Admin:</b> Hi Farhad!
+      </span>
+      <span className="sentMessageStyle p-1 mx-4 my-2">
+        <b className="has-text-danger-dark">Admin:</b> We see this is your first
+        time chatting with us.
+      </span>
+      <span className="sentMessageStyle p-1 mx-4 my-2">
+        <b className="has-text-danger-dark">Admin:</b> Welcome to Narvan group.
+      </span>
     </div>
   );
-}
-
+};
+export default ChatContent;
